aa627dec57ee14d833b8b663c8c43e99b5515fb9
NarekTelunts21 <narek_t@instigatemobile.com>
2022-04-15 18:33:16 +0400

    Changes1

--- Narek1.py
+++ Narek1.py
@@ -1,5 +1,5 @@
-def avg(marks):
-    assert len(marks) != 0, "The marks length is 0"
-    return sum(marks)/len(marks)
-mark1 = []
-print("Average of mark1:",avg(mark1))
+x = 3
+if x >= 5:
+    print("true")
+else:
+    print (asdasda)
